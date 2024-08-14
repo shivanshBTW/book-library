226d22f0453edcec2301c1bc252f72c2300055ac
Shivansh Tyagi <stshivansh9@gmail.com>
2024-08-14 14:00:03 +0530

    added page items logic to pagination

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -1,24 +1,33 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 interface PaginationProps<T> {
   itemList: T[];
   maxItemsPerPage?: number;
 }
 
+interface PaginationReturn<T> {
+  pageItems: T[];
+  totalPages: number;
+  currentPage: number;
+  setCurrentPage: (page: number) => void;
+  goToNextPage: () => void;
+  goToPreviousPage: () => void;
+  isPaginationRequired: boolean;
+  isFirstPage: boolean;
+  isLastPage: boolean;
+}
+
 function usePagination<T>({
   itemList,
   maxItemsPerPage = 10,
-}: PaginationProps<T>) {
-  const [items, setItems] = useState(itemList);
+}: PaginationProps<T>): PaginationReturn<T> {
+  const [pageItems, setPageItems] = useState<Array<T>>([]);
   const [currentPage, setCurrentPage] = useState(1);
 
-  const isPaginationRequired = items.length > maxItemsPerPage;
-  const totalPages = Math.ceil(items.length / maxItemsPerPage);
+  const isPaginationRequired = itemList.length > maxItemsPerPage;
+  const totalPages = Math.ceil(itemList.length / maxItemsPerPage);
 
   const startElementIndex = (currentPage - 1) * maxItemsPerPage;
-  const pageItems: T[] = !isPaginationRequired
-    ? items
-    : items.slice(startElementIndex, startElementIndex + maxItemsPerPage);
 
   const isFirstPage = currentPage === 1;
   const isLastPage = currentPage === totalPages;
@@ -30,6 +39,21 @@ function usePagination<T>({
   const goToPreviousPage = () => {
     if (!isFirstPage) setCurrentPage(currentPage - 1);
   };
+
+  const getPageItems = useCallback(() => {
+    if (!isPaginationRequired) return itemList;
+
+    const pageItems: T[] = itemList.slice(
+      startElementIndex,
+      startElementIndex + maxItemsPerPage
+    );
+
+    return pageItems;
+  }, [itemList, isPaginationRequired, startElementIndex, maxItemsPerPage]);
+
+  useEffect(() => {
+    setPageItems(getPageItems());
+  }, [itemList, currentPage, getPageItems]);
 
   return {
     pageItems,
